9be13da176255f6600c307ed34518a4bcc134b86
LostAlien96 <aayushmangal001@gmail.com>
2025-06-27 19:52:32 +0530

    Initial commit

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,11 @@
+import express from express
+import { protectRoute } from "../middleware/auth.middleware";
+import {getMyFriends,getRecommendedUsers} from "../controllers/user.controller"
+const router=express.Router();
+router.use(protectRoute)
+router.get("/",getRecommendedUsers);
+router.get("/friends",getMyFriends);
+router.post("/friend-request/:id",sendFriendRequest);
+router.post("/friend-request/:id/accept",acceptFriendRequest);
+
+export default router;
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+ 
+using namespace std;
+int main(){
+   long long  ans=1;
+   for(int i=0;i<1000;i++)ans*=2;
+
+   cout<<ans;
+}
